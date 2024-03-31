427c6cc61bdce29335c8f8eb86c5eb2e80b3303e
Uniekideasconsulting <uniekideas0@gmail.com>
2024-03-31 13:54:28 +0100

    feedback updated

--- js/search.js
+++ js/search.js
@@ -32,3 +32,39 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('mobilesearchInput');
+    const itemList = document.getElementById('mobileitemList');
+    const noResultsMessage = document.getElementById('noResultsMessage');
+    const items = Array.from(itemList.getElementsByTagName('li'));
+
+    searchInput.addEventListener('input', function () {
+        const searchTerm = searchInput.value.toLowerCase();
+        let hasResults = false;
+
+        if (searchTerm === '') {
+            items.forEach(item => {
+                item.classList.add('hidden');
+            });
+        } else {
+            items.forEach(item => {
+                const itemText = item.textContent.toLowerCase();
+                if (itemText.includes(searchTerm)) {
+                    item.classList.remove('hidden');
+                    hasResults = true;
+                } else {
+                    item.classList.add('hidden');
+                }
+            });
+        }
+
+        // Show/hide noResultsMessage based on search results
+        if (hasResults) {
+            noResultsMessage.classList.add('hidden');
+        } else {
+            noResultsMessage.classList.remove('hidden');
+        }
+    });
+});
