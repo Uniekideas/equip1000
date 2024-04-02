14674c9ac665ac94340bad7965ed531582c51b4f
Azeez Fasasi <67533381+Azeezfasasi@users.noreply.github.com>
2024-04-02 16:24:32 +0100

    Updated

--- js/messaging.js
+++ js/messaging.js
@@ -1,7 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Run this code when the DOM is ready
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Run this code when the DOM is ready
   
-    // Show Inbox content by default
+//     // Show Inbox content by default
+//     openTab('Inbox');
+//   });
+  
+//   function openTab(tabName) {
+//     var i;
+//     var tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     var tablinks = document.getElementsByClassName("tablink");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].classList.remove("active");
+//     }
+//     document.getElementById(tabName).style.display = "block";
+//     event.currentTarget.classList.add("active");
+//   }
+  
+
+//   document.addEventListener("DOMContentLoaded", function() {
+//     // Run this code when the DOM is ready
+  
+//     // Show Inbox content by default
+//     openTab('NewMessage');
+//   });
+  
+//   function openTab(tabName) {
+//     var i;
+//     var tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     var tablinks = document.getElementsByClassName("tablink");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].classList.remove("active");
+//     }
+//     document.getElementById(tabName).style.display = "block";
+//     event.currentTarget.classList.add("active");
+//   }
+
+  
+  document.addEventListener("DOMContentLoaded", function() {
     openTab('Inbox');
   });
   
@@ -16,28 +57,5 @@ document.addEventListener("DOMContentLoaded", function() {
       tablinks[i].classList.remove("active");
     }
     document.getElementById(tabName).style.display = "block";
-    event.currentTarget.classList.add("active");
+    document.querySelector('.' + tabName.toLowerCase() + '-tablink').classList.add("active");
   }
-  
-
-  document.addEventListener("DOMContentLoaded", function() {
-    // Run this code when the DOM is ready
-  
-    // Show Inbox content by default
-    openTab('NewMessage');
-  });
-  
-  function openTab(tabName) {
-    var i;
-    var tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    var tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove("active");
-    }
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.classList.add("active");
-  }
-  
